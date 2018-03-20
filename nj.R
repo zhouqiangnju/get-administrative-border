4ffac5483f1d1793d1bf2d71ebb551fc1aa19414
zhouqiangnju <zhouqiangnju@163.com>
2018-03-20 17:40:29 +0800

    completion

--- nj.R
+++ nj.R
@@ -1,31 +1,29 @@
-library("httr") 
-
-library("magrittr") 
+library("httr")
+library("magrittr")
 library("jsonlite")
-library(Rgctc)
+library(Rgctc2)
 library("dplyr")
 library('plyr')
 library('sf')
-library(zoo)
+
 library(maptools)
 library(rgdal)
 library(stringr)
 library(rlist)
 library(ggplot2)
 options(digits=11)
-get_location<- function(address){  
+get_location<- function(address){
   key = '7c6b6c0d1b641f4aa9cdb7d2229ae728'
-  
   url = 'http://restapi.amap.com/v3/config/district?' %>%
-    paste('keywords=' , address ,  
+    paste('keywords=' , address ,
           '&key=' ,key ,
-          '&subdistrict=3' , 
+          '&subdistrict=3' ,
           '&extensions=all',
-          sep = '')  
+          sep = '')
   city<-GET(url)%>% content(as="text",encoding="UTF-8") %>% fromJSON(flatten = TRUE)
   return(city)
 }
-nj<-get_location('320106')[[6]]
+nj<-get_location('南京')[[6]]
 nj_district<-nj$districts[[1]]
 district_adcode<-nj_district$adcode
 district_info<-nj_district$districts
@@ -35,74 +33,47 @@ town_center <- district_info%>% lapply(select,name,center,level) %>% list.rbind
 town_center$district<-row.names(town_center)
 #制作各区行政区划图，
 #1、提取行政区划边界及中心坐标点信息
-names(district_frame)<-nj_district$name
+
 district_frame<- lapply(district_adcode,get_location) %>% list.map(districts)
+names(district_frame)<-nj_district$name
 district_center<-district_frame%>% lapply(select,adcode,name,center,level) %>% list.rbind
-gc[428]
-gc[429]
-gc[429] %>% str_replace('\\|',';')
-polyline<-lapply(district_frame,select,polyline) 
-gc<-polyline[[1]] %>% str_split('\\|') %>%lapply(str_split,';') 
-gc2<-list.apply(gc,list.apply,str_split,',')
-gc[duplicated(gc)]
-gc[1]==gc[428]
-district_polyline<- lapply(district_frame,select,polyline) %>% lapply(str_split ,';') %>% lapply('[[',1) %>% lapply(str_split,',')
-grep_verse<-function(x,y){grep(pattern = y,x=x)} # change parameter order of grep to meet the need of lapply
-odd_po<-lapply(odd,grep_verse,3)
-for (i in 1:length(district_polyline)){
-  if (length(odd[[i]])>0){
-    for(j in 1:length(odd[[i]])){
-      
-    }
-    
-  }
-    
-    else{ 
-      district_polyline[[i]]<-district_polyline[[i]] %>% lapply(as.numeric) %>% list.rbind
-      district_center$frame[i]<-data.frame(lng_wgs84=
-                                             gcj02_wgs84_lng(district_polyline[[i]][,1],district_polyline[[i]][,2]),
-                                           lat_wgs84=
-                                             gcj02_wgs84_lat(district_polyline[[i]][,1],district_polyline[[i]][,2])) %>%
-                                apply(1,list)%>% lapply(unlist)%>%list.rbind%>%list%>% 
-                                st_polygon
-    }
-}
 
+#2. 提取Level-0级行政区行政区划边界点坐标信息并转换成面对象
+nj<-get_location('南京')  %>% '[['(6) %>% select(-districts)
 
+nj$polyline<-nj$polyline %>% str_split('\\|') %>% lapply(str_split,';')  %>%'[['(1)%>%
+          lapply(lapply,str_split,',')%>%
+          lapply(lapply,lapply,as.numeric)%>% lapply(list.rbind)  %>% lapply(list.rbind)%>%
+          lapply(gcj02_wgs84_matrix_matrix) %>% lapply(list)%>% #  由GCJ02坐标系转换为WGS84坐标系
+          st_multipolygon %>%                                  #定义为sf类中的复多边形（multipolygon)对象
+          st_sfc(crs=4326)                                     #将该列定义为sfc列，并将投影坐标系属性设定为WGS84投影
+nj.sf<-st_sf(nj)                                               #将整个数据框定义为st对象
+nj.sf$center<-nj.sf$center %>% str_split(',')  %>% lapply(as.numeric)
+nj.sf$center_wgs84_lng<-gcj02_wgs84_lng(nj.sf$center[[1]][1],nj.sf$center[[1]][2])
+nj.sf$center_wgs84_lat<-gcj02_wgs84_lat(nj.sf$center[[1]][1],nj.sf$center[[1]][2])
 
-x=matrix(1:12,3,4)
-row(x)
-js_frame<-js$polyline %>% str_split(';') %>% lapply(str_split,',') %>% '[['(1)
-t<-list.apply(js_frame,length)%>% unlist
-odd<-which(t==3)
-js_frame<-unlist(js_frame)
-js_frame[[odd]]
-odd<-list.findi(js_frame,length(.)==3)
+#3. 提取Level-1级行政区区划边界点坐标信息并转换成面对象
+nj_district<-get_location('南京') %>% '[['('districts') %>% '[['('districts') %>% '[['(1)
+district_poly <-lapply(nj_district$adcode,get_location) %>% list.map(districts) %>% lapply(select,polyline)
+names(district_poly) <-nj_district$name
+nj_district$poly<-district_poly %>% lapply(str_split,'\\|') %>% lapply(lapply,str_split,';') %>% lapply('[[',1)%>%
+      lapply(lapply,str_split,',') %>% lapply(lapply,lapply,as.numeric) %>% lapply(lapply,list.rbind)%>%
+      lapply(lapply,gcj02_wgs84_matrix_matrix) %>% lapply(lapply,list) %>%
+      lapply(st_multipolygon) %>% st_sfc(crs=4326)
+nj_district.sf<-st_sf(nj_district)
 
-frame_1<-frame%>% list.subset(1:odd-1)
-frame_1[[30]]<-frame_1[[1]]
-frame_1 <- frame_1 %>% lapply(as.numeric) %>% list.rbind %>% list %>% st_polygon %>% st_sfc(crs=4326)
- frame_1<-          st_sf(id='nj_1',geometry=frame_1)
+#4. 提取Level-2级行政区地理信息
+nj_town<-nj_district %>% select(districts) %>% '[['(1) %>% lapply(select,-districts) %>% list.rbind
+center<-nj_town$center %>% str_split(',')  %>% lapply(as.numeric)  %>% list.apply(rbind)%>%
+                lapply(unlist) %>% list.rbind %>% gcj02_wgs84_matrix_df
+nj_town<-cbind.data.frame(nj_town,center)
 
-frame_2<-frame%>% list.subset(31:length(frame)) %>%
-         list.insert(1,frame[[length(frame)]])%>%lapply(as.numeric)%>%
-         list.rbind %>% list %>%
-         st_polygon %>% st_sfc(crs=4326)
-frame_2<-          st_sf(id='nj_2',geometry=frame_2)
-frame_nj<-list.merge(frame_1,frame_2)
+nj_town$center<-center %>% apply(1,list) %>% lapply (unlist) %>% lapply(st_point) %>% st_sfc(crs=4326)
+nj_town.sf<-nj_town %>% st_sf
+ggplot()+geom_sf(data=nj_town.sf)
+setwd('F:/Administrator/Documents/R/Mapproject/JSframe/njframe')
+saveRDS(nj_district.sf,'nj_district_sf.rds')
+saveRDS(nj_town.sf,'nj_town_sf.rds')
+#3. 转换坐标系
 
-frame_nj<-list()
-frame_nj[[1]]<-frame_1
-frame_nj[[2]]<-frame_2  
-ggplot()+geom_sf(data=frame_1)+geom_sf(data=frame_2)
-
-frame<-frame_n
-frame<-list.exclude(frame,length(.)==3)
-frame<-frame %>% lapply(as.numeric) %>% list.rbind
-write.csv(frame,'frame.csv')
-frame<-rbind(frame,frame[1,]) %>% list %>% st_polygon %>% st_sfc(crs=4326)
-nj.frame<-st_sf(id='nj',geometry=frame)
-ggplot()+geom_sf(data=nj.frame)
-t<-list.apply(frame,length)
-nj<-lapply(nj_d$name,get_location) %>% list.map(districts)
-nj_d<-get_location('南京') %>%  '[['(6) %>% '[['(7) %>% '[['(1) %>% select('name','center')
+#4.
